8c6adcc64dafa100e77de4ba0aab7c702736e817
no-inspi <71600397+no-inspi@users.noreply.github.com>
2025-09-02 21:07:40 +0200

    test deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    esmExternals: "loose",
+  },
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), "motion"];
+    return config;
+  },
 };
 
 export default nextConfig;
